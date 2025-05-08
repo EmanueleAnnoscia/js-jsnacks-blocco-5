b175f188e302bdb693ee89bdf3eadd11d14f476c
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-05-08 14:04:06 +0200

    esercizio 8 completato

--- js-jsnacks-blocco-5/snack-8/script.js
+++ js-jsnacks-blocco-5/snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classStudent = students.find((curStudent) => curStudent.name === "Marco Lanci")
+
+console.log(classStudent.class)
