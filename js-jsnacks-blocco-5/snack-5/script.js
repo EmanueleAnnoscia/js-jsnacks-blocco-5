0126171719d7c93f07131fffaa008f56ea02005a
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-05-08 13:48:40 +0200

    esercizio 5 completato

--- js-jsnacks-blocco-5/snack-5/script.js
+++ js-jsnacks-blocco-5/snack-5/script.js
@@ -2,8 +2,16 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
-const evenArr=[]
+const evenArr=[];
 
-const evenNumbers = nums.filter((curNumb) => evenArr.push(curNumb % 2 === 0))
+// const evenNumbers = nums.filter((curNumb) => evenArr.push(curNumb % 2 === 0))
 
-console.log(evenArr)
+
+
+nums.forEach((curNumb, i)=>{
+    if(curNumb % 2 === 0){
+        evenArr.push(curNumb)
+    }
+});
+
+console.log(evenArr);
