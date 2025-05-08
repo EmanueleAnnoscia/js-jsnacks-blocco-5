694c50aeffc9c2fe2b2a93aa5d3f8d2e887041f9
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-05-08 13:24:15 +0200

    esercizio 4 completato

--- js-jsnacks-blocco-5/snack-4/script.js
+++ js-jsnacks-blocco-5/snack-4/script.js
@@ -7,3 +7,5 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const nameArr = posts.map((curAuthor) => console.log(curAuthor.author))
